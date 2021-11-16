3f54fe11df65d6c1c0fbef460838f6caf041b79e
Vincent Curran <vincentcurran@Vincents-MacBook-Pro-2.local>
2021-11-16 14:08:31 -0700

    initial commit

---
+++ gamepiece.h
@@ -0,0 +1,13 @@
+#ifndef GAMEPIECE_H
+#define GAMEPIECE_H
+
+#include <QObject>
+
+class GamePiece
+{
+    Q_OBJECT
+public:
+    GamePiece();
+};
+
+#endif // GAMEPIECE_H
---
+++ tile.h
@@ -0,0 +1,13 @@
+#ifndef TILE_H
+#define TILE_H
+
+#include <QObject>
+
+class Tile
+{
+    Q_OBJECT
+public:
+    Tile();
+};
+
+#endif // TILE_H
