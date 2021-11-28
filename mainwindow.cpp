07b2e760f87559849f514f0b7ae70e52e7a90568
Philip <philip@cyberline.com>
2021-11-28 16:51:45 -0700

    Shortened turn label updating

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -52,11 +52,8 @@ MainWindow::MainWindow(QWidget *parent)
 
 // update turn label with whoevers turn it is
 void MainWindow::updateTurnLabel(int turn) {
-    if (!turn) {
-        ui->turnLabel->setText("TURN: RED");
-    } else {
-        ui->turnLabel->setText("TURN: BLACK");
-    }
+    QString turn_str = turn ? "BLACK" : "RED";
+    ui->turnLabel->setText("TURN: " + turn_str);
 }
 
 // update label displaying how many pieces a player has
